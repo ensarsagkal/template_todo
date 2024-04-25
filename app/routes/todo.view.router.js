6a7e5f9c19297941e0595cf39b7b162156dff88d
ensar <ensarsagkal@gmail.com>
2024-04-25 20:40:09 +0300

    up

--- app/routes/todo.view.router.js
+++ app/routes/todo.view.router.js
@@ -19,18 +19,17 @@ const router = require('express').Router()
 //     .delete(todo.delete)
 
 // router.get('/', todo.list)
- router.all('/', todo.list)
+router.all('/', todo.list)
 
 // router.get('/create', todo.create) // form view
 // router.post('/create', todo.create) // form send
 router.all('/create', todo.create)
 
-router.all('/id', todo.read)
+router.all('/:id', todo.read)
 
-router.all('/id/update', todo.update)
+router.all('/:id/update', todo.update)
 
-router.all('/id/delete', todo.delete)
-
+router.all('/:id/delete', todo.delete)
 
 
 module.exports = router
