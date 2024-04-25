8ac5133e6ac2b7ca3fb0859feb9fb323d4656403
ensar <ensarsagkal@gmail.com>
2024-04-25 09:34:11 +0300

    up

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ require("dotenv").config()
 const PORT=process.env.PORT || 8000
 app.use(express.json())
 require("express-async-errors")
+//* TEMPLATE-EJS
+app.set("view engine","ejs")
+
 app.use(require("./app/routes/todo.router")) //& ROUTER SAYFASI OLUŞTUKTAN SONRA YOL BELİRLENECEK
 app.use(require("./app/errorHandler")) //& ERROR SAYFASI OLUŞTUKTAN SONRA YOL BELİRLENECEK +
 app.listen(PORT,()=>console.log("Running: https://127.0.0.1:" + PORT))
