66139c02f342cfe17da1efbae65d4e45c90300a6
ensar <ensarsagkal@gmail.com>
2024-04-25 12:58:31 +0300

    up

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
-"use strict"
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - TODO Project with Sequelize
+------------------------------------------------------- */
 
+const express = require("express");
+const app = express();
 
-const express=require("express")
-const app=express()
-require("dotenv").config()
-const PORT=process.env.PORT || 8000
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
 
-
+/* ------------------------------------------------------- */
+// Accept json data and convert object:
 app.use(express.json())
 
-require("express-async-errors")
+// Accept form-urlencoded and convert to object:
+app.use(express.urlencoded({ extended: true }))
 
-//* TEMPLATE-EJS
-app.set("view engine","ejs")
+// Catch async-errors:
+require('express-async-errors')
+/* ------------------------------------------------------- */
+//* TEMPLATE - EJS
+// $ npm i ejs
+// https://ejs.co/
+// https://www.npmjs.com/package/ejs
+// https://github.com/mde/ejs/wiki/Using-EJS-with-Express
 
-app.set("views","./public")
+// Setting template engine:
+// console.log(app)
+app.set('view engine', 'ejs')
 
-app.all("/",(req,res) =>{
+// default template folder: ./views/
+app.set('views', './public')
+
+app.all('/', (req, res) => {
+    // API:
+    // res.send({
+    //     message: 'Hello'
+    // })
+    // View Template:
+    // call ejs file in ./views/
+    // res.render('index.ejs')
     res.render('index')
 })
 
+/* ------------------------------------------------------- */
+// Routes:
 
+app.use('/api', require('./app/routes/todo.router'))
+app.use('/view', require('./app/routes/todo.view.router'))
 
-
-app.use("/api",require("./app/routes/todo.router")) 
-app.use("/views",require("./app/routes/todo.view.router")) 
-app.use(require("./app/errorHandler"))
-app.listen(PORT,()=>console.log("Running: https://127.0.0.1:" + PORT))
+/* ------------------------------------------------------- */
+// ErrorHandler:
+app.use(require('./app/errorHandler'))
+/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
