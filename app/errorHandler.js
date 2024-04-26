22b0a8d06d7f2b4d92f21ada16ea089f6c1de26c
ensar <ensarsagkal@gmail.com>
2024-04-26 14:06:44 +0300

    up

--- app/errorHandler.js
+++ app/errorHandler.js
@@ -1,11 +1,27 @@
-"use strict"
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - TODO Project with Sequelize
+------------------------------------------------------- */
 
-module.exports=(err,req,res,next)=>{
-    const errorStatusCode=res.errorStatusCode ?? 500
-    console.log("errorHandle worked!")
-    res.status(errorStatusCode).send({
-        error:true,
-        message:err.message,
-        cause:err.cause
-    })
+module.exports = (err, req, res, next) => {
+    const errorStatusCode = res.errorStatusCode ?? 500
+
+    console.log('errorHandler worked.')
+
+    // console.log(req.originalUrl)
+
+    const data = {
+        error: true, // special data
+        message: err.message, // error string message
+        cause: err.cause, // error option cause
+        // stack: err.stack, // error details
+    }
+
+    if (req.originalUrl.startsWith('/api')) {
+    // API -> JSON:
+        res.status(errorStatusCode).send(data)
+    } else {
+    // VIEW -> HTML
+        res.render('errors', { data })
+    }
 }
